ddbfff81ed8e08d46065444106ac385da252c538
Chris Lawver <chris_lawver@yahoo.com>
2021-01-29 14:51:01 -0500

    Completed assignment

--- CarGarage/Car.cs
+++ CarGarage/Car.cs
@@ -12,5 +12,70 @@ namespace CarGarage
         public int FuelLevel { get; set; }
         public bool AutoBrakeActive { get; set; }
 
+
+        public string SetMake(string make)
+        {
+            Make = make;
+            return make;
+        }
+
+        public string SetModel(string model)
+        {
+            Model = model;
+            return model;
+        }
+
+        public int GetSpeed()
+        {
+            return Speed;
+        }
+
+        public bool IsSpeeding()
+        {
+            if (Speed > 70)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int Accelerate()
+        {
+            if(Speed > 90)
+            {
+                return 100;
+            }
+            else if (Make == "Ferrari")
+            {
+                return Speed += 20;
+            }
+            else
+            {
+            return Speed += 10;
+            }
+        }
+
+        public int Brake()
+        {
+            if (Speed < 7)
+            {
+                return Speed = 0;
+            }
+            else if (AutoBrakeActive)
+            {
+                return Speed /= 2;
+            }
+            else
+            {
+                return Speed -= 7;
+            }
+        }
+
+
+
+
     }
 }
